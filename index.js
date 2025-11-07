b3f105f60b632566676497426471a99c37a1541e
MahmoudKhaled007 <85295806+MahmoudKhaled007@users.noreply.github.com>
2025-11-07 20:47:05 +0200

    ds

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-<<<<<<< Updated upstream
 // Use "type": "module" in package.json to use ES module imports
-import { createServer } from 'http';
-
-const PORT = process.env.PORT || 3000;
+import { createServer } from "http";
 
 const server = createServer((req, res) => {
+  const PORT = process.env.PORT || 3000;
   // for example: GET /is-alive
-  if (req.method === 'GET' && req.url === '/is-alive') {
-    
+  if (req.method === "GET" && req.url === "/is-alive") {
     // Read the value from Vercel's Environment Variables.
     // process.env variables are always strings, so we compare to the string "true".
     // Default to `true` if the variable isn't set.
-    const isOk = process.env.API_STATUS_OK ? (process.env.API_STATUS_OK === 'true') : true;
+    const isOk = process.env.API_STATUS_OK
+      ? process.env.API_STATUS_OK === "true"
+      : true;
 
     res.writeHead(200, {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     });
     res.end(JSON.stringify({ ok: isOk }));
   } else {
     // any other route
-    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ ok: false }));
   }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(3000, () => {
+  console.log(`Server running on http://localhost:${3000}`);
 });
-=======
->>>>>>> Stashed changes
